58ffe506d0ff948c579aa1c19719896d8b4c47d0
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-08 18:03:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   obj[key] = value;
   return obj;
 }
+
+function deleteFromObjectByKey(obj, key) {
+  var newobj = Object.assign( {}, obj);
+  delete newobj[key];
+  return newobj;
+}
