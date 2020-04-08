9ddc0164a160dc3887789035c230ad121219e034
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-08 17:58:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function updateObjectWithKeyAndValue(obj, key, value) {
-  var newobj = Object.assign( {}, obj, {key : value});
+  var newobj = Object.assign( {}, obj, {[key] : value});
   return newobj;
 }
 
